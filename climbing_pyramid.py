0014af36479c871f9ef3163172374b5f2b0e1948
Melissa Thai <mthai95@gmail.com>
2022-05-17 17:29:02 -0700

    sort climbing pyramid by grade

--- climbing_pyramid.py
+++ climbing_pyramid.py
@@ -4,7 +4,17 @@ import csv
 from collections import defaultdict
 
 DEFAULT_ROUTE_TYPE = "sport"
-LEAD_SENDS = ["Redpoint", "Onsight"]
+GRADES_ORDER = [
+    "3rd", "4th", "5.0", "5.1", "5.2", "5.3",
+    "5.4", "5.5", "5.6", "5.7", "5.8", "5.9",
+    "5.10-", "5.10a", "5.10b", "5.10", "5.10+", "5.10c", "5.10d",
+    "5.11-", "5.11a", "5.11b", "5.11", "5.11+", "5.11c", "5.11d",
+    "5.12-", "5.12a", "5.12b", "5.12", "5.12+", "5.12c", "5.12d",
+    "5.13-", "5.13a", "5.13b", "5.13", "5.13+", "5.13c", "5.13d",
+    "5.14-", "5.14a", "5.14b", "5.14", "5.14+", "5.14c", "5.14d",
+    "5.15-", "5.15a", "5.15b", "5.15", "5.15+", "5.15c", "5.15d",
+]
+LEAD_SENDS = ["Redpoint", "Onsight", "Flash", "Pinkpoint"]
 PROTECTION_RATINGS = ["PG13", "R", "X"]
 TICKS_FILE = "ticks.csv"
 
@@ -44,12 +54,19 @@ def is_requested_route_type(requested_route_type: str, current_route_type: str) 
 def print_climbing_pyramid(climbing_pyramid: dict):
     import pprint
 
-    pp = pprint.PrettyPrinter(indent=4)
+    pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
     pp.pprint(climbing_pyramid)
 
 
+def sort_by_grade(climbing_pyramid: dict) -> dict:
+    climbing_pyramid_sorted = {
+        key: climbing_pyramid[key] for key in GRADES_ORDER if key in climbing_pyramid}
+
+    return climbing_pyramid_sorted
+
+
 if __name__ == "__main__":
-    climbing_pyramid_dictionary = defaultdict(set)
+    climbing_pyramid = defaultdict(set)
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -62,7 +79,8 @@ if __name__ == "__main__":
         reader = csv.DictReader(csvfile)
         for row in reader:
             if is_requested_route_type(route_type, row["Route Type"]) and row["Lead Style"] in LEAD_SENDS:
-                climbing_pyramid_dictionary[get_grade(
-                    row["Rating"])].add(row["Route"])
+                climbing_pyramid[get_grade(row["Rating"])].add(row["Route"])
 
-    print_climbing_pyramid(climbing_pyramid_dictionary)
+    climbing_pyramid_sorted = sort_by_grade(climbing_pyramid)
+
+    print_climbing_pyramid(climbing_pyramid_sorted)
